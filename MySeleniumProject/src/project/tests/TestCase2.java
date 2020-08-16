21356ade467e52870ccdd58cccbc064aed50213c
demogit123 <sureshbabu.qedge@gmail.com>
2020-08-16 18:50:12 +0530

    Update TestCase2.java

--- MySeleniumProject/src/project/tests/TestCase2.java
+++ MySeleniumProject/src/project/tests/TestCase2.java
@@ -5,7 +5,8 @@ public class TestCase2 {
 	public static void main(String[] args) 
 	{
 		System.out.println("This is TestCase2");
-
+		System.out.println("TestCase2 Modified by other Team Member");
+		
 	}
 
 }
